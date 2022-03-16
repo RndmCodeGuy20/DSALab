17f18592d55cb687b26e710166916039e6857561
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2022-03-16 10:08:23 +0530

    chore: :building_construction: tried flexxing my skills
    could not flex, sorry future shantanu

--- DSA Viva Final/AscendPriorQ.cpp
+++ DSA Viva Final/AscendPriorQ.cpp
@@ -1,9 +1,70 @@
 #include <iostream>
 using namespace std;
 
+struct node
+{
+    int priority;
+    int data;
+    struct node *link;
+};
+
+void enQ(struct node *ptr, int priority, int data)
+{
+    struct node *newNode;
+    struct node *trail = ptr;
+    newNode = (struct node *)malloc(sizeof(struct node));
+
+    newNode->data = data;
+    newNode->priority = priority;
+    newNode->link = NULL;
+
+    if (ptr == NULL)
+    {
+        ptr = newNode;
+    }
+
+    if (ptr != NULL)
+    {
+        if (ptr->priority > newNode->priority)
+        {
+            newNode->link = ptr;
+            ptr = newNode;
+        }
+
+        while (ptr)
+        {
+            ptr = ptr->link;
+
+            if (ptr->priority > newNode->priority)
+            {
+                trail->link = newNode;
+                newNode->link = ptr;
+            }
+
+            trail = ptr;
+        }
+    }
+}
+
+void dispList(struct node *ptr)
+{
+    while (ptr)
+    {
+        printf("\t|  %d   |   %d   |   🔗 \t", ptr->data, ptr->priority);
+        ptr = ptr->link;
+    }
+}
+
 int main()
 {
-    
+    struct node *head = NULL;
+
+    enQ(head, 3, 392);
+    enQ(head, 2, 39);
+    enQ(head, 4, 2);
+    enQ(head, 1, 92);
+
+    dispList(head);
 
     return 0;
 }
